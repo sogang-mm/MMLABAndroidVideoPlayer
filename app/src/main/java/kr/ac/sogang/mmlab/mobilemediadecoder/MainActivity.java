39881aa2c028053cc6bed14c4989b9c6ee8192cc
송진하 <thejn07@gmail.com>
2020-07-06 05:36:11 +0900

    테스트 코드 추가(MediaMetaRetriever 사용) * VideoView에서 동영상 재생 중 capture 버튼을 누르면 영상의 current position을 millisecond로 불러옴 * 불러온 current position으로 MediaMetaRetriever을 이용해 해당 시간의 frame을 bitmap으로 가져옴 -> thread를 통해 자동으로 매초 frame을 저장하도록 수정

--- app/src/main/java/kr/ac/sogang/mmlab/mobilemediadecoder/MainActivity.java
+++ app/src/main/java/kr/ac/sogang/mmlab/mobilemediadecoder/MainActivity.java
@@ -1,9 +1,25 @@
 package kr.ac.sogang.mmlab.mobilemediadecoder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.media.MediaMetadataRetriever;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,6 +50,65 @@ public class MainActivity extends AppCompatActivity {
                 requestPermissions(PERMISSIONS, PERMISSIONS_REQUEST_CODE);
             }
         }
+
+        String video_path = "file://" + Environment.getExternalStorageDirectory().toString() + "/Download/test_1080p.mp4";
+        File files = new File(video_path);
+        if(files.exists()==true) {
+            Log.e("VIDEO_NAME", video_path + " exist");
+        } else {
+            Log.e("VIDEO_NAME", "Not exist");
+        }
+
+        videoView = (VideoView) findViewById(R.id.videoView);
+        buttonCapture = (Button) findViewById(R.id.capture);
+        videoView.setVideoURI(Uri.parse(video_path));
+        mediaMetadataRetriever = new MediaMetadataRetriever();
+        mediaMetadataRetriever.setDataSource(MainActivity.this, Uri.parse(video_path));
+        myMediaController = new MediaController(this);
+        videoView.setMediaController(myMediaController);
+
+        videoView.setOnCompletionListener(videoViewCompletionListener);
+        videoView.setOnPreparedListener(videoViewPreparedListener);
+        videoView.setOnErrorListener(videoViewErrorListener);
+
+        videoView.requestFocus();
+        videoView.start();
+        buttonCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                int currentPosition = videoView.getCurrentPosition(); //in millisecond
+                Toast.makeText(MainActivity.this, "Current Position: " + currentPosition + " (ms)", Toast.LENGTH_LONG).show();
+                Bitmap bmFrame = mediaMetadataRetriever.getFrameAtTime(currentPosition * 1000); //unit in microsecond
+                Log.e("BITMAP", "witdth: " + Integer.toString(bmFrame.getWidth()) + " / height: " + Integer.toString(bmFrame.getHeight()));
+
+            }
+        });
+    }
+
+    MediaPlayer.OnCompletionListener videoViewCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer arg0) {
+            Toast.makeText(MainActivity.this, "End of Video", Toast.LENGTH_LONG).show();
+        }
+    };
+
+    MediaPlayer.OnPreparedListener videoViewPreparedListener = new MediaPlayer.OnPreparedListener() {
+        @Override
+        public void onPrepared(MediaPlayer mp) {
+            long duration = videoView.getDuration(); //in millisecond
+            Toast.makeText(MainActivity.this, "Duration: " + duration + " (ms)", Toast.LENGTH_LONG).show();
+
+        }
+    };
+
+    MediaPlayer.OnErrorListener videoViewErrorListener = new MediaPlayer.OnErrorListener() {
+        @Override
+        public boolean onError(MediaPlayer mp, int what, int extra) {
+            Toast.makeText(MainActivity.this, "Error!!!", Toast.LENGTH_LONG).show();
+            return true;
+        }
+    };
+
     private boolean hasPermissions(String[] permissions) {
         int result;
 
