5adb72ebcbd79df397001b69adf592f988027b64
송진하 <thejn07@gmail.com>
2020-08-31 08:26:30 +0900

    FFmpegWapper 수정 - CNN inference base model class 추가 - 시간 측정을 위한 debuging log 추가 - ffmpeg hwaccel option 추가

---
+++ app/src/main/java/kr/ac/sogang/mmlab/AndroidVideoPlayer/filter/BaseModel.java
@@ -0,0 +1,17 @@
+package kr.ac.sogang.mmlab.AndroidVideoPlayer.filter;
+
+import android.graphics.Bitmap;
+
+public class BaseModel {
+
+    // TODO: Model initialization
+    public BaseModel() {
+
+    }
+
+    // TODO: Inference function
+    public boolean inference(Bitmap bitmap) {
+
+        return true;
+    }
+}
